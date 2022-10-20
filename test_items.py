49418898b01397c1c81b7e7a13bc05b004a27a71
artsartone <brainxkhaled@mail.ru>
2022-10-20 16:39:54 +0300

    already

---
+++ test_items.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_basket_btn(browser):
+    browser.get(link)
+    time.sleep(30)
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket"), 'button add to basket not found'
